1bb0984304c26363d48cc2da1d34c85733cfb8f9
Sindhu12-ks <koratlasindhu1@gmail.com>
2026-04-02 15:37:35 +0530

    just small changes

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/MiniGames">
+    <BrowserRouter basename="/MiNi-Games">
       <App />
     </BrowserRouter>
   </React.StrictMode>
